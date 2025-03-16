7e9aba5ea833ace6c96dea0e82374722a5ec9105
Caarl001 <leonardovenegas763@gmail.com>
2025-03-16 07:11:19 -0300

    Clase 207 - Sección 11

--- Clases y Objetos/Aritmetica.py
+++ Clases y Objetos/Aritmetica.py
@@ -1,29 +1,49 @@
 class Aritmetica:
 
     def __init__(self, operando1 , operando2):
-        self.operando1 = operando1
-        self.operando2 = operando2
+        self._operando1 = operando1
+        self._operando2 = operando2
 
     def sumer(self):
-        resultado = self.operando1 + self.operando2
+        resultado = self._operando1 + self._operando2
         print(f'Resultado de la suma: {resultado}')
 
     def resta(self):
-        resultado = self.operando1 - self.operando2
+        resultado = self._operando1 - self._operando2
         print(f'Resultado de la resta: {resultado}')
 
     def multiplicar(self):
-        resultado = self.operando1 * self.operando2
+        resultado = self._operando1 * self._operando2
         print(f'Resultado de la multiplicación: {resultado}')
 
     def division(self):
-        resultado = self.operando1 / self.operando2
+        resultado = self._operando1 / self._operando2
         print(f'Resultado de la division: {resultado}')
+
+    @property
+    def operando1(self):
+        return self._operando1
+
+    @operando1.setter
+    def operando1(self, operando1):
+        self._operando1 = operando1
+
+    @property
+    def operando2(self):
+        return self._operando2
+
+    @operando2.setter
+    def operando2(self, operando2):
+        self._operando2 = operando2
+
 
 # Programa principal
 if __name__ == '__main__':
     print(' --- Ejemplo Clase Aritmetica ---')
     aritmetica1 = Aritmetica(5 , 7)
+    print('Primer objeto')
+    print(f'Valor operando1 del objeto aritmetica1: {aritmetica1.operando1}')
+    print(f'Valor operando2 del objeto aritmetica2: {aritmetica1.operando2}')
     aritmetica1.sumer()
     aritmetica1.resta()
     aritmetica1.multiplicar()
@@ -31,6 +51,8 @@ if __name__ == '__main__':
 
     # Otro objeto
     aritmetica2 = Aritmetica(12, 16)
+    print(f'Valor operando1 del objeto aritmetica1: {aritmetica2.operando1}')
+    print(f'Valor operando2 del objeto aritmetica2: {aritmetica2.operando2}')
     aritmetica2.sumer()
     aritmetica2.resta()
     aritmetica2.multiplicar()
